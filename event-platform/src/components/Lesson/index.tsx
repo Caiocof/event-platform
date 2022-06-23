37c44add1a2da48b74e7179393e7daaede4aa291
caiocof <caiooliveira3652@outlook.com>
2022-06-23 12:17:31 -0300

    feat: create Video and Lesson, add new librarys

--- event-platform/src/components/Lesson/index.tsx
+++ event-platform/src/components/Lesson/index.tsx
@@ -1,6 +1,7 @@
 import { CheckCircle, Lock } from 'phosphor-react'
 import { isPast, format } from 'date-fns'
 import ptBr from 'date-fns/locale/pt-BR'
+import { Link } from 'react-router-dom';
 
 interface LessonProps {
    title: string;
@@ -19,9 +20,9 @@ export const Lesson = ({ title, slug, availableAt, type }: LessonProps) => {
       })
 
    return (
-      <a href="#" className="text-gray-300">
+      <Link to={`/event/lesson/${slug}`} className="group" >
          <span>{availableDataFormat}</span>
-         <div className="rounded border border-gray-500 p-4 mt-2">
+         <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
             <header className="flex items-center justify-between">
                {isLessonAvailable ? (
                   <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
@@ -42,7 +43,7 @@ export const Lesson = ({ title, slug, availableAt, type }: LessonProps) => {
                {title}
             </strong>
          </div>
-      </a>
+      </Link>
 
    )
 }
